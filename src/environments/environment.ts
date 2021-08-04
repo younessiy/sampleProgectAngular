02ca56da759c884d5abb55ea3dcef371de71967d
younessiysa <youness.iysa@gmail.com>
2021-08-04 15:27:30 +0100

    signalR

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44363/api/'
+  apiUrl: 'https://localhost:44363/api/',
+  hubUrl: 'https://localhost:44363/hubs/'
 };
 
 /*
